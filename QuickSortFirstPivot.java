73cc61c48273bbcce62eb236b26c024f6a4bbf75
ZuyuLiao <42748792+ZuyuLiao@users.noreply.github.com>
2020-12-13 20:59:35 -0600

    Update QuickSortFirstPivot.java

--- QuickSortFirstPivot.java
+++ QuickSortFirstPivot.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class QuickSortFirstPivot {
-    public void quicksort(int[] nums, int low, int high){
+    public static void quicksort(int[] nums, int low, int high){
         if(low < high){
             int pivot = partition(nums, low, high);
             quicksort(nums, low, pivot-1);
@@ -9,20 +9,20 @@ class QuickSortFirstPivot {
         }
     }
 
-    public int partition(int[] nums, int low, int high){
-        int pivot = low+1;
-        for(int j = low+1;j<nums.length;j++){
-            if(nums[j] < nums[low]){
+    public static int partition(int[] nums, int low, int high){
+        int pivot = low-1;
+        for(int j = low; j < high;j++){
+            if(nums[j] < nums[high]){
+                pivot++;
                 int temp = nums[pivot];
                 nums[pivot] = nums[j];
                 nums[j] = temp;
-                pivot++;
             }
         }
-        int temp1 = nums[low];
-        nums[low] = nums[pivot-1];
-        nums[pivot-1] = temp1;
-        return pivot-1;
+        int temp = nums[pivot+1];
+        nums[pivot+1] = nums[high];
+        nums[high] = temp;
+        return pivot+1;
     }
 
     public static void main(String[] args) {
